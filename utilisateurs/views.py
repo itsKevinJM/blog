7f9c412f6f62327b6f04d73cba29d4138a587755
Jean Marie Kévin <kjeanmarie01@gmail.com>
2022-12-21 16:12:57 +0100

    ok

--- utilisateurs/views.py
+++ utilisateurs/views.py
@@ -5,4 +5,9 @@ from django.http import HttpResponse
 
 
 def utilisateurs_view(request):
-    return HttpResponse("Page d'utilisateurs")
+    # return HttpResponse("Page d'utilisateurs")
+    return render(request, 'utilisateurs/liste.html')
+
+
+def creer_view(request):
+    return HttpResponse('page création utilisateur')
